2671309cc08f7ea6a149d17c81818ba0791b1489
eas <eas@mailbox.org>
2022-08-11 15:36:52 +0200

    argument conversion logic

--- inc/args.hpp
+++ inc/args.hpp
@@ -4,10 +4,12 @@
 #include "overload.hpp"
 
 #include <any>
+#include <charconv>
 #include <optional>
 #include <ostream>
 #include <span>
 #include <string_view>
+#include <unordered_map>
 #include <variant>
 #include <vector>
 
@@ -44,7 +46,7 @@ struct flag_match
 // interpretation depends on what options take values and which are valueless flags.
 args_map extract_args(char** argv, std::span<flag_match> flags);
 
-constexpr auto replace = []<typename T>(T&&, T&& second) { return std::forward<T>(second); };
+constexpr auto replace = []<typename T>(T, T second) { return std::forward<T>(second); };
 
 template <typename T>
 struct named
@@ -54,12 +56,11 @@ struct named
   std::string_view id;
   std::string_view lname { id };
   char             sname { lname[0] };
-
-  std::optional<T> flag {};
   std::optional<T> init {};
-  T (*reduce)(T&&, T&&) { replace };
+  std::optional<T> flag {};
+  T (*reduce)(T, T) { replace };
 
-  bool             required { false };
+  std::string_view alt { id };
   std::string_view help {};
 };
 
@@ -69,8 +70,10 @@ struct positional
   using value_type = T;
 
   std::string_view id;
+  std::optional<T> init {};
+  T (*reduce)(T, T) { replace };
 
-  bool             required { false };
+  std::string_view alt { id };
   std::string_view help {};
 };
 
@@ -81,7 +84,155 @@ struct opt_result
 };
 
 struct parse_result
-{};
+{
+  enum status_e : unsigned char
+  {
+    good = 0,
+    warn = 1,
+    err  = 2,
+  } status { good };
+
+  std::vector<std::string>                       messages;
+  std::string                                    synopsis;
+  std::unordered_map<std::string_view, std::any> opts;
+
+  void raise(status_e s, std::string&& msg)
+  {
+    if ( s > status ) { status = s; }
+    messages.push_back(msg);
+  }
+
+  template <typename T>
+  std::optional<T> getopt(std::string_view id)
+  {
+    return std::any_cast<std::optional<T>>(opts.at(id));
+  }
+};
+
+template <typename T>
+struct value_conv;
+
+template <typename T>
+concept arithmetic = std::floating_point<T> || std::integral<T>;
+
+template <arithmetic T>
+struct value_conv<T>
+{
+  std::optional<T> operator()(std::string_view sv)
+  {
+    T number;
+
+    auto res { std::from_chars(sv.begin(), sv.end(), number) };
+    if ( res.ec != std::errc {} ) { return {}; }
+
+    return number;
+  }
+};
+
+template <>
+struct value_conv<std::string_view> : std::identity
+{
+  using std::identity::operator();
+};
+
+// This is a lambda because it's used in an overload{}
+constexpr auto match_named =
+  []<typename T>(parse_result& result, const args_map::arg_entry& arg, const named<T>& opt)
+{
+  // Main test here. Is this the option we're looking for?
+  if ( (arg.first.size() == 1 && arg.first.starts_with(opt.sname)) || arg.first == opt.lname )
+  {
+    if ( opt.flag && arg.second )
+    {
+      result.raise(parse_result::err, std::string(arg.first) + " takes no argument");
+    }
+    else if ( !opt.flag && !arg.second )
+    {
+      result.raise(parse_result::err, std::string(arg.first) + " needs argument");
+    }
+    else if ( opt.flag )
+    {
+      auto old_val { std::any_cast<std::optional<T>>(result.opts[opt.id]) };
+      if ( old_val ) { result.opts[opt.id] = std::optional<T> { opt.reduce(*old_val, *opt.flag) }; }
+      else { result.opts[opt.id] = opt.flag; }
+    }
+    else
+    {
+      auto val { value_conv<T> {}(*arg.second) };
+      if ( !val )
+      {
+        result.raise(
+          parse_result::err,
+          std::string(arg.first) + " can't recognize argument '" + std::string(*arg.second) + "'");
+      }
+      else
+      {
+        auto old_val { std::any_cast<std::optional<T>>(result.opts[opt.id]) };
+        if ( old_val ) { result.opts[opt.id] = std::optional<T> { opt.reduce(*old_val, *val) }; }
+        else { result.opts[opt.id] = val; }
+      }
+    }
+
+    return true;
+  }
+  return false;
+};
+
+template <typename... Ts>
+std::string synopsis(std::string_view app_name, Ts... opts)
+{
+  std::string msg { std::string { app_name } + ' ' };
+
+  std::string flags_string {};
+
+  const auto posix_flag = overload {
+    [&]<typename T>(const named<T>& o)
+    {
+      if ( o.sname && o.flag ) { flags_string.append({ o.sname }); }
+    },
+    [](const auto&) {},
+  };
+  (..., posix_flag(opts));
+
+  if ( !flags_string.empty() ) { msg += std::string { "[-" } + flags_string + "] "; }
+
+  const auto gnu_flag = overload {
+    [&]<typename T>(const named<T>& o)
+    {
+      if ( !o.sname && !o.lname.empty() && o.flag )
+      {
+        msg += std::string { "[--" } + std::string { o.lname } + "] ";
+      }
+    },
+    [](const auto&) {},
+  };
+  (..., gnu_flag(opts));
+
+  const auto valued = overload {
+    [&]<typename T>(const named<T>& o)
+    {
+      if ( !o.flag && (o.sname || !o.lname.empty()) )
+      {
+        msg += '[';
+        if ( o.sname ) { msg += std::string { '-' } + o.sname; }
+        if ( o.sname && !o.lname.empty() ) { msg += '|'; }
+        if ( !o.lname.empty() ) { msg += std::string { "--" } + std::string { o.lname }; }
+        msg += std::string { ' ' } + std::string { o.alt } + "] ";
+      }
+    },
+    [](const auto&) {},
+  };
+  (..., valued(opts));
+
+  const auto operands = overload {
+    [&]<typename T>(const positional<T>& o)
+    { msg += std::string { '<' } + std::string { o.id } + "> "; },
+    [](const auto&) {},
+  };
+  (..., operands(opts));
+
+  return msg;
+};
 
 template <typename... Ts>
 auto parse(char** argv, Ts... opts)
@@ -93,12 +244,34 @@ auto parse(char** argv, Ts... opts)
     {
       if ( o.flag ) { posix_flags.push_back({ o.sname, o.lname }); }
     },
-    []<typename T>(const positional<T>& p) {},
+    [](const auto& p) {},
   };
   (..., push_if_flag(opts));
 
-  auto args { extract_args(argv, posix_flags) };
-  return args;
+  auto         args { extract_args(argv, posix_flags) };
+  parse_result result;
+
+  result.synopsis = synopsis(args.invocation, opts...);
+
+  const auto init_results = [&](const auto& opt) { result.opts.emplace(opt.id, opt.init); };
+
+  (..., init_results(opts));
+
+  const auto test_named = overload {
+    match_named,
+    [](auto&, const auto&, const auto&) { return false; },
+  };
+
+  for ( const auto& arg : args.named )
+  {
+    bool matched = (... || test_named(result, arg, opts));
+    if ( !matched )
+    {
+      result.raise(parse_result::err, "option '" + std::string(arg.first) + "' not known");
+    }
+  }
+
+  return result;
 }
 
 } // namespace args
--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,28 @@ int main(int /*argc*/, char** argv)
 {
   auto args = args::parse(
     argv,
-    args::named<int> { .id = "use-alt", .sname = 'a', .flag = 2 },
-    args::positional<std::string_view> { .id = "help" });
+    args::named<float> { .id    = "point-sdev",
+                         .sname = 'p',
+                         .init  = std::numeric_limits<float>::max(),
+                         .alt   = "float",
+                         .help  = "maximum accepted standard deviation for point weights." },
+    args::named<float> { .id    = "face-sdev",
+                         .sname = 'f',
+                         .init  = std::numeric_limits<float>::max(),
+                         .alt   = "float",
+                         .help  = "maximum accepted standard deviation for face weights." },
+    args::positional<std::string_view> { .id   = "shape",
+                                         .help = "die to process. d6/d8/d10/d12/d20" });
 
-  std::cout << args << '\n';
+  if ( args.status != args.good )
+  {
+    std::cerr << "issues:\n";
+    for ( const auto& m : args.messages ) { std::cerr << " - " << m << '\n'; }
+    std::cerr << "\nusage: " << args.synopsis << "\n\n";
+    return EXIT_FAILURE;
+  }
+
+  std::cout << args.getopt<float>("point-sdev").value() << '\n';
 
   const auto start_time { std::chrono::steady_clock::now() };
   std::cerr << "Hello.\n";
